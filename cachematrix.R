e9b0187fa0367dee4f407f70c898e2b99fed4b68
Rithvik Vinekar <rsvinekar@gmail.com>
2023-02-04 23:41:00 +0530

    Assignment tested and ready
    changed email address: rsvinekar@gmail.com

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## Map the variables holding matrix and inverse to a variable outside ths scope
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'.
+## If cached value is present, use that value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
+
+## Testing
+## Create Matrix
+# > x <- matrix(c(1,2,2,3,1,2,3,3,1),3,3)
+
+## Create Cache
+# > X <- makeCacheMatrix(x)
+
+## Solve the matrix - first run - no cache yet
+# > cacheSolve(X)
+#            [,1]       [,2]       [,3]
+# [1,] -0.3846154  0.2307692  0.4615385
+# [2,]  0.3076923 -0.3846154  0.2307692
+# [3,]  0.1538462  0.3076923 -0.3846154
+
+## Cache has been created
+## Solve the matrix - second run
+# > cacheSolve(X)
+# getting cached data
+#            [,1]       [,2]       [,3]
+# [1,] -0.3846154  0.2307692  0.4615385
+# [2,]  0.3076923 -0.3846154  0.2307692
+# [3,]  0.1538462  0.3076923 -0.3846154
+
+## "getting cached data" - it works!!!
